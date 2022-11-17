c38e4c3f1a542dffd81c595648d2eb158936bb61
Francerz <francerzam@hotmail.com>
2022-11-17 10:40:32 -0600

    CHG removed unused use classes

--- app/Controllers/AbstractController.php
+++ app/Controllers/AbstractController.php
@@ -2,10 +2,6 @@
 
 namespace App\Controllers;
 
-use App\Application;
-use Fig\Http\Message\StatusCodeInterface as IStatusCode;
-use Psr\Http\Message\ResponseInterface as IResponse;
-
 abstract class AbstractController
 {
 }
